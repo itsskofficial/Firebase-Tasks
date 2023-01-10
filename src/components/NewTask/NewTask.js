6bde5ae8d66d7d6632cd73910026870e08773631
SK <sarthakkarandikar03@gmail.com>
2023-01-10 08:23:21 +0000

    Jan 10, 2023, 1:53 PM

--- src/components/NewTask/NewTask.js
+++ src/components/NewTask/NewTask.js
@@ -5,7 +5,12 @@ import Section from '../UI/Section';
 import TaskForm from './TaskForm';
 
 const NewTask = (props) => {
-  const createTask = ()
+  const createTask = (taskData) => {
+    const generatedId = data.name; // firebase-specific => "name" contains generated id
+    const createdTask = { id: generatedId, text: taskText };
+
+    props.onAddTask(createdTask);
+  }
 
   const enterTaskHandler = async (taskText) => {
       const response = useHttp({url:'https://react-http-6b4a6.firebaseio.com/tasks.json'},
