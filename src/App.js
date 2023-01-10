7e1201e1347eb0c454e8041e8d94aff72a09dc95
SK <sarthakkarandikar03@gmail.com>
2023-01-10 07:47:41 +0000

    Jan 10, 2023, 1:17 PM

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import useHttp from './hooks/useHttp';
 function App() {
   const response = useHttp({
     url:'https://tasks-54c86-default-rtdb.firebaseio.com/tasks.json',
-  })
+  }, transformTasks)
 
 
   const [tasks, setTasks] = useState([]);
