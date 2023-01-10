c09d9e670ace1283c05d5d5b4e401bf6b639f145
SK <sarthakkarandikar03@gmail.com>
2023-01-10 01:08:40 +0000

    Jan 10, 2023, 6:38 AM

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
     setError(null);
     try {
       const response = await fetch(
-        'https://react-http-6b4a6.firebaseio.com/tasks.json'
+        'https://tasks-54c86-default-rtdb.firebaseio.com/tasks.json'
       );
 
       if (!response.ok) {
